2126d00d0a95ee909bb8cc716268a16ace22ce63
徐悟源 <359696929@qq.com>
2017-08-29 17:11:21 +0800

    add apollo

--- src/main/kotlin/com/wuyuan/KotlinJetFlightApplication.kt
+++ src/main/kotlin/com/wuyuan/KotlinJetFlightApplication.kt
@@ -2,8 +2,10 @@ package com.wuyuan
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ImportResource
 
 @SpringBootApplication
+@ImportResource
 class KotlinJetFlightApplication
 
 fun main(args: Array<String>) {
