42ba090deebf6b7c2259c56254a3f7a89ff1ab02
徐悟源 <359696929@qq.com>
2017-08-30 09:38:45 +0800

    set cluster default

--- src/main/kotlin/com/wuyuan/KotlinJetFlightApplication.kt
+++ src/main/kotlin/com/wuyuan/KotlinJetFlightApplication.kt
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ImportResource
 
 @SpringBootApplication
 @ImportResource
-@EnableApolloConfig("application")
+@EnableApolloConfig("application", "default")
 class KotlinJetFlightApplication
 
 fun main(args: Array<String>) {
